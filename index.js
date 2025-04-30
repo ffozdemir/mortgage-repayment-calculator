f7cd1f2c97195a595d8f7476831bcf4dfc36a3b0
Fetullah Furkan Özdemir <furkanozdemir51@yandex.com.tr>
2025-04-30 16:45:37 +0200

    style: add error handling and validation for mortgage form inputs, enhance layout with error messages

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const form = document.querySelector("form");
 const resultSection = document.querySelector(".result-section");
+const beforeResult = document.querySelector(".before-result");
+const monthlyRepayments = document.querySelector("#monthly-repayments");
+const repayOverTheTerm = document.querySelector("#repay-over-the-term");
+const resultDiv = document.querySelector(".result");
+const mortgageAmountInputEl = document.querySelector("#mortgage-amount");
+const mortgageTermInputEl = document.querySelector("#mortgage-term");
+const interestInputEl = document.querySelector("#interest-rate");
+const mortgageInputGroupEl = document.querySelector(".mortgage-input-group");
+const mortgageTermGroupEl = document.querySelector(".mortgage-term-group");
+const interestRateGroupEl = document.querySelector(".interest-rate-group");
+const radioButtons = document.querySelectorAll('input[name="mortgage-type"]');
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -8,6 +19,42 @@ form.addEventListener("submit", (event) => {
   const mortgageTerm = formData.get("mortgage-term");
   const interestRate = formData.get("interest-rate");
   const mortgageType = formData.get("mortgage-type");
+
+  if (!mortgageAmount || !mortgageTerm || !interestRate || !mortgageType) {
+    if (!mortgageAmount) {
+      addErrorClass(
+        mortgageInputGroupEl,
+        mortgageAmountInputEl,
+        "input-error-mortgage"
+      );
+    }
+    if (!mortgageTerm) {
+      addErrorClass(
+        mortgageTermGroupEl,
+        mortgageTermInputEl,
+        "input-error-term"
+      );
+    }
+    if (!interestRate) {
+      addErrorClass(interestRateGroupEl, interestInputEl, "input-error-rate");
+    }
+    if (!mortgageType) {
+      form.querySelector("#second-radio").nextElementSibling.textContent =
+        "This field is required.";
+    }
+    return;
+  }
+  if (
+    isNaN(mortgageAmount) ||
+    isNaN(mortgageTerm) ||
+    isNaN(interestRate) ||
+    parseFloat(mortgageAmount) <= 0 ||
+    parseInt(mortgageTerm) <= 0 ||
+    parseFloat(interestRate) <= 0
+  ) {
+    return;
+  }
+
   const data = {
     loanAmount: parseFloat(mortgageAmount),
     term: parseInt(mortgageTerm),
@@ -16,7 +63,28 @@ form.addEventListener("submit", (event) => {
   };
   const monthlyRepayment = calculateMonthlyRepayment(data);
   const totalRepayment = calculateTotalRepayment(monthlyRepayment, data.term);
-  console.log(monthlyRepayment.toFixed(2), totalRepayment.toFixed(2));
+
+  monthlyRepayments.textContent = monthlyRepayment.toFixed(2);
+  repayOverTheTerm.textContent = totalRepayment.toFixed(2);
+  resultDiv.classList.remove("disactive");
+  beforeResult.classList.replace("before-result", "disactive");
+});
+
+document.querySelector('button[type="reset"]').addEventListener("click", () => {
+  resultDiv.classList.add("disactive");
+  beforeResult.classList.replace("disactive", "before-result");
+  removeErrorClass(
+    mortgageInputGroupEl,
+    mortgageAmountInputEl,
+    "input-error-mortgage"
+  );
+  removeErrorClass(
+    mortgageTermGroupEl,
+    mortgageTermInputEl,
+    "input-error-term"
+  );
+  removeErrorClass(interestRateGroupEl, interestInputEl, "input-error-rate");
+  form.querySelector("#second-radio").nextElementSibling.textContent = "";
 });
 
 const calculateMonthlyRepayment = (data) => {
@@ -41,20 +109,36 @@ const calculateMonthlyRepayment = (data) => {
 const calculateTotalRepayment = (monthlyRepayment, term) =>
   monthlyRepayment * term * 12;
 
-const renderResult = () => {
-  return `<div class="result">
-          <h2>Your results</h2>
-          <p>
-            Your results are shown below based on the information you provided.
-            To adjust the results, edit the form and click “calculate
-            repayments” again.
-          </p>
-          <div class="result-card">
-            <p>Your monthly repayments</p>
-            <p id="monthly-repayments">£1,797.74</p>
-            <hr />
-            <p>Total you'll repay over the term</p>
-            <p id="repay-over-the-term">£539,322.94</p>
-          </div>
-        </div>`;
+const addErrorClass = (groupEl, inputEl, errorClass) => {
+  groupEl.nextElementSibling.textContent = "This field is required.";
+  inputEl.classList.add(errorClass);
+  inputEl.previousElementSibling.style.color = "var(--color-white) !important";
 };
+const removeErrorClass = (groupEl, inputEl, errorClass) => {
+  groupEl.nextElementSibling.textContent = "";
+  inputEl.classList.remove(errorClass);
+  inputEl.previousElementSibling.style.color = "var(--color-slate-500)";
+};
+mortgageAmountInputEl.addEventListener("input", () => {
+  removeErrorClass(
+    mortgageInputGroupEl,
+    mortgageAmountInputEl,
+    "input-error-mortgage"
+  );
+});
+mortgageTermInputEl.addEventListener("input", () => {
+  removeErrorClass(
+    mortgageTermGroupEl,
+    mortgageTermInputEl,
+    "input-error-term"
+  );
+});
+interestInputEl.addEventListener("input", () => {
+  removeErrorClass(interestRateGroupEl, interestInputEl, "input-error-rate");
+});
+
+radioButtons.forEach((radio) => {
+  radio.addEventListener("change", () => {
+    document.querySelector("#second-radio").nextElementSibling.textContent = "";
+  });
+});
